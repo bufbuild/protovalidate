2fd04533388092e715b06800ab336901ddaa19bc
Chris Roche <github@rodaine.com>
2023-05-25 12:34:29 -0700

    Initial commit
    Co-authored-by: Elliot Jackson <13633636+elliotmjackson@users.noreply.github.com>
    Co-authored-by: Alfred Fuller <afuller@buf.build>
    Co-authored-by: Tony Li <122298175+tonyli233@users.noreply.github.com>

---
+++ Makefile
@@ -0,0 +1,102 @@
+# See https://tech.davis-hansson.com/p/make/
+SHELL := bash
+.DELETE_ON_ERROR:
+.SHELLFLAGS := -eu -o pipefail -c
+.DEFAULT_GOAL := all
+MAKEFLAGS += --warn-undefined-variables
+MAKEFLAGS += --no-builtin-rules
+MAKEFLAGS += --no-print-directory
+BIN := .tmp/bin
+COPYRIGHT_YEARS := 2023
+LICENSE_IGNORE := -e internal/testdata/
+# Set to use a different compiler. For example, `GO=go1.18rc1 make test`.
+GO ?= go
+ARGS ?=
+
+.PHONY: help
+help: ## Describe useful make targets
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "%-15s %s\n", $$1, $$2}'
+
+.PHONY: all
+all: test lint ## Run all tests and lint (default)
+
+.PHONY: clean
+clean: ## Delete intermediate build artifacts
+	@# -X only removes untracked files, -d recurses into directories, -f actually removes files/dirs
+	git clean -Xdf
+
+.PHONY: test
+test: generate ## Run all unit tests
+	$(GO) test -race -cover ./tools/...
+	bazel test //...
+
+.PHONY: lint
+lint: lint-proto lint-go  ## Lint code and protos
+
+.PHONY: lint-go
+lint-go: $(BIN)/golangci-lint
+	$(BIN)/golangci-lint run ./tools/...
+
+.PHONY: lint-proto
+lint-proto: $(BIN)/buf
+	$(BIN)/buf lint
+	$(BIN)/buf breaking --against '.git#branch=main'
+
+.PHONY: conformance
+conformance: ## Build conformance harness
+	$(GO) build -o $(BIN)/protovalidate-conformance ./tools/protovalidate-conformance
+
+.PHONY: generate
+generate: generate-bazel generate-proto generate-license ## Regenerate code and license headers
+
+.PHONY: bazel
+bazel: generate-bazel
+	bazel build //...
+
+.PHONY: generate-bazel
+generate-bazel:
+	bazel run //:gazelle
+
+.PHONY: generate-proto
+generate-proto: $(BIN)/buf
+	rm -rf tools/internal/gen/*/
+	$(BIN)/buf generate
+
+.PHONY: generate-license
+generate-license: $(BIN)/license-header
+	@# We want to operate on a list of modified and new files, excluding
+	@# deleted and ignored files. git-ls-files can't do this alone. comm -23 takes
+	@# two files and prints the union, dropping lines common to both (-3) and
+	@# those only in the second file (-2). We make one git-ls-files call for
+	@# the modified, cached, and new (--others) files, and a second for the
+	@# deleted files.
+	comm -23 \
+		<(git ls-files --cached --modified --others --no-empty-directory --exclude-standard | sort -u | grep -v $(LICENSE_IGNORE) ) \
+		<(git ls-files --deleted | sort -u) | \
+		xargs $(BIN)/license-header \
+			--license-type apache \
+			--copyright-holder "Buf Technologies, Inc." \
+			--year-range "$(COPYRIGHT_YEARS)"
+
+.PHONY: checkgenerate
+checkgenerate: generate
+	@# Used in CI to verify that `make generate` doesn't produce a diff.
+	test -z "$$(git status --porcelain | tee /dev/stderr)"
+
+.PHONY: upgrade-go
+upgrade-go:
+	cd ./tools/internal/gen && $(GO) get -u -t ./... && go mod tidy -v
+	cd ./tools && $(GO) get -u -t ./... && go mod tidy -v
+
+$(BIN):
+	@mkdir -p $(BIN)
+
+$(BIN)/buf: $(BIN) Makefile
+	GOBIN=$(abspath $(@D)) $(GO) install github.com/bufbuild/buf/cmd/buf@latest
+
+$(BIN)/license-header: $(BIN) Makefile
+	GOBIN=$(abspath $(@D)) $(GO) install \
+		  github.com/bufbuild/buf/private/pkg/licenseheader/cmd/license-header@latest
+
+$(BIN)/golangci-lint: $(BIN) Makefile
+	GOBIN=$(abspath $(@D)) $(GO) install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
---
+++ tools/protovalidate-conformance/internal/suites/suite.go
@@ -0,0 +1,126 @@
+// Copyright 2023 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package suites
+
+import (
+	"regexp"
+
+	harness2 "github.com/bufbuild/protovalidate/tools/internal/gen/buf/validate/conformance/harness"
+	results2 "github.com/bufbuild/protovalidate/tools/protovalidate-conformance/internal/results"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+type Suites map[string]Suite
+
+func (ss Suites) Range(filter *regexp.Regexp, fn func(suiteName string, suite Suite) error) error {
+	for suiteName, suite := range ss {
+		if filter != nil && !filter.MatchString(suiteName) {
+			continue
+		}
+		if err := fn(suiteName, suite); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type Suite map[string]Case
+
+func (s Suite) Range(filter *regexp.Regexp, fn func(caseName string, testCase Case) error) error {
+	for caseName, testCase := range s {
+		if filter != nil && !filter.MatchString(caseName) {
+			continue
+		}
+		if err := fn(caseName, testCase); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s Suite) ToRequestProto(filter *regexp.Regexp) (*harness2.TestConformanceRequest, error) {
+	bldr := &fdsetBuilder{seen: map[string]struct{}{}}
+	req := &harness2.TestConformanceRequest{
+		Fdset: &bldr.fdest,
+		Cases: map[string]*anypb.Any{},
+	}
+
+	return req, s.Range(filter, func(caseName string, testCase Case) error {
+		bldr.addFile(testCase.Message.ProtoReflect().Descriptor().ParentFile())
+		anyCase, err := anypb.New(testCase.Message)
+		req.Cases[caseName] = anyCase
+		return err
+	})
+}
+
+func (s Suite) ProcessResults(
+	suiteName string,
+	filter *regexp.Regexp,
+	resp *harness2.TestConformanceResponse,
+	verbose bool,
+	strict bool,
+) *results2.SuiteResults {
+	out := &results2.SuiteResults{
+		Name: suiteName,
+	}
+
+	respResults := resp.GetResults()
+	_ = s.Range(filter, func(caseName string, testCase Case) error {
+		var actual results2.Result
+		if result, ok := respResults[caseName]; ok {
+			actual = results2.FromProto(result)
+		} else {
+			actual = results2.UnexpectedError(
+				"missing results on response")
+		}
+
+		out.AddCase(&harness2.CaseResult{
+			Name:    caseName,
+			Success: testCase.Expected.IsSuccessWith(actual, strict),
+			Wanted:  testCase.Expected.ToProto(),
+			Got:     actual.ToProto(),
+		}, verbose)
+		return nil
+	})
+
+	return out
+}
+
+type fdsetBuilder struct {
+	seen  map[string]struct{}
+	fdest descriptorpb.FileDescriptorSet
+}
+
+func (b *fdsetBuilder) addFile(desc protoreflect.FileDescriptor) {
+	if _, seen := b.seen[desc.Path()]; seen {
+		return
+	}
+
+	for i, imports := 0, desc.Imports(); i < imports.Len(); i++ {
+		b.addFile(imports.Get(i))
+	}
+
+	b.fdest.File = append(b.fdest.File, protodesc.ToFileDescriptorProto(desc))
+	b.seen[desc.Path()] = struct{}{}
+}
+
+type Case struct {
+	Message  proto.Message
+	Expected results2.Result
+}
